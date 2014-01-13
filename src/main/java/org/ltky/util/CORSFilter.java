22f4c215500e94d7e372f00e15d14ac242ee8bf8
Laastine <mikko.kaistinen@lut.fi>
2014-01-13 18:13:35 +0200

    CORS filter added

---
+++ src/main/java/org/ltky/util/CORSFilter.java
@@ -0,0 +1,32 @@
+package org.ltky.util;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * lukkarimaatti
+ * Created with IntelliJ IDEA.
+ * User: laastine
+ * Date: 13.1.2014
+ */
+public class CORSFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+            response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with, sid, mycustom, smuser");
+            response.addHeader("Access-Control-Max-Age", "900"); //15min
+        }
+        filterChain.doFilter(request, response);
+    }
+}

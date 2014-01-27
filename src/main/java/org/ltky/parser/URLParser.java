ef362f2750db5ec2103927950735238b54884db3
Laastine <mikko.kaistinen@lut.fi>
2014-01-27 20:05:16 +0200

    Irrelevant imports removed

--- src/main/java/org/ltky/parser/URLParser.java
+++ src/main/java/org/ltky/parser/URLParser.java
@@ -7,7 +7,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.*;
+import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Iterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 

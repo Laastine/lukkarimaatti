196e97643253d9c7ced92ea0b7f6db2930679fe2
Laastine <mikko.kaistinen@lut.fi>
2014-03-22 21:48:37 +0200

    Course name prefetch works

--- src/main/java/org/ltky/dao/daoImpl/CourseDaoImpl.java
+++ src/main/java/org/ltky/dao/daoImpl/CourseDaoImpl.java
@@ -65,7 +65,7 @@ public class CourseDaoImpl implements CourseDao {
     }
 
     public List<Course> findCourseNames(String courseName) {
-        final String hql = "FROM Course C WHERE lower(C.courseName) like lower(:courseName)";
+        final String hql = "SELECT C.courseName FROM Course C WHERE lower(C.courseName) like lower(:courseName)";
         Query query = getCurrentSession().createQuery(hql);
         List<Course> list = query.setParameter("courseName", courseName + "%").list();
         return list;

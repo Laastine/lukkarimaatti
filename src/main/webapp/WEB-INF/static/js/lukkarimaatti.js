8f5df45068cff057713c244a2e36acc0acaf95b3
Laastine <mikko.kaistinen@lut.fi>
2014-05-16 18:22:40 +0300

    JS libs reorganized and JS-files splitted to two separated files

--- src/main/webapp/WEB-INF/static/js/lukkarimaatti.js
+++ src/main/webapp/WEB-INF/static/js/lukkarimaatti.js
@@ -1,4 +1,4 @@
-var Lukkarimaatti = (function () {
+Lukkarimaatti = (function () {
     'use strict';
     var courseNames, environment, noppa;
     courseNames = null;
@@ -8,7 +8,6 @@ var Lukkarimaatti = (function () {
     $.ajaxPrefilter(function (options, originalOptions, jqXHR) {
         options.url = environment + options.url;
     });
-
 
     var view = {};
 
@@ -203,69 +202,10 @@ var Lukkarimaatti = (function () {
         });
     })();
 
-    var searchQuery = $(function () {
-        var courses;
-        var engine = new Bloodhound({
-                datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-                queryTokenizer: Bloodhound.tokenizers.whitespace,
-                remote: {
-                    url: '/rest/cnames/%QUERY',
-                    filter: function (response) {
-                        // parsedResponse is the array returned from your backend
-                        console.log(JSON.stringify(response));
-                        courses = $.map(response, function (course) {
-                                return {
-                                    title: course.courseName,
-                                    code: course.courseCode,
-                                    tof: course.timeOfDay,
-                                    wd: course.weekDay,
-                                    wn: course.weekNumber,
-                                    cr: course.classRoom,
-                                    t: course.type
-                                };
-                            }
-                        );
-                        return courses;
-                    }
-                },
-                limit: 5
-            }
-        );
+    new Search();
 
-        engine.initialize();
-        $('#courseSearchBox').typeahead({
-                hint: true,
-                highlight: true,
-                minLength: 3
-            },
-            {
-                name: 'courses',
-                displayKey: 'name',
-                source: engine.ttAdapter(),
-                templates: {
-                    empty: [
-                        '<p><strong>',
-                        'Unable to find any courses that match the current query',
-                        '</strong></p>'
-                    ].join('\n'),
-                    suggestion: Handlebars.compile('<p><strong>{{title}}</strong> - {{code}}</p>')
-                }
-            }
-        );
 
-        $('#addButton').click(function () {
-            function processCourse(course) {
-                var h = course.tof.split('-')[0] || 6;
-                var date = moment().day(course.wd || 'su').week(course.wn || '21').hours(h).minutes(0).second(0).format('YYYY-MM-DDTHH:mm:ssZ');
-                console.log('date='+date);
-                view.createCalendarEvent(course, date);  //Render via backbone
-            }
-            courses.forEach(processCourse);
-        });
-
-    });
-
-}());
+})();
 
 /*
 
---
+++ src/main/webapp/WEB-INF/static/js/search.js
@@ -0,0 +1,61 @@
+Search = (function () {
+    var courses;
+    var engine = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            remote: {
+                url: '/rest/cnames/%QUERY',
+                filter: function (response) {
+                    // parsedResponse is the array returned from your backend
+                    console.log(JSON.stringify(response));
+                    courses = $.map(response, function (course) {
+                            return {
+                                title: course.courseName,
+                                code: course.courseCode,
+                                tof: course.timeOfDay,
+                                wd: course.weekDay,
+                                wn: course.weekNumber,
+                                cr: course.classRoom,
+                                t: course.type
+                            };
+                        }
+                    );
+                    return courses;
+                }
+            },
+            limit: 5
+        }
+    );
+
+    engine.initialize();
+    $('#courseSearchBox').typeahead({
+            hint: true,
+            highlight: true,
+            minLength: 3
+        },
+        {
+            name: 'courses',
+            displayKey: 'name',
+            source: engine.ttAdapter(),
+            templates: {
+                empty: [
+                    '<p><strong>',
+                    'Unable to find any courses that match the current query',
+                    '</strong></p>'
+                ].join('\n'),
+                suggestion: Handlebars.compile('<p><strong>{{title}}</strong> - {{code}}</p>')
+            }
+        }
+    );
+
+    $('#addButton').click(function () {
+        function processCourse(course) {
+            var h = course.tof.split('-')[0] || 6;
+            var date = moment().day(course.wd || 'su').week(course.wn || '21').hours(h).minutes(0).second(0).format('YYYY-MM-DDTHH:mm:ssZ');
+            console.log('date='+date);
+            view.createCalendarEvent(course, date);  //Render via backbone
+        }
+        courses.forEach(processCourse);
+    });
+
+})();

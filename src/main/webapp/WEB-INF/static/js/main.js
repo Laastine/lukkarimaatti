d91e1dbe0183f7fdadf772785db67665dae5be3f
Laastine <mikko.kaistinen@lut.fi>
2014-06-29 20:37:52 +0300

    Course events gets rendered via AMD loading

--- src/main/webapp/WEB-INF/static/js/main.js
+++ src/main/webapp/WEB-INF/static/js/main.js
@@ -1,13 +1,18 @@
 require(['js/views/eventCalendarView', 'js/search'], function(EventCalendarView, search) {
     'use strict';
-    // Just use GET and POST to support all browsers
-    Backbone.emulateHTTP = true;
 
-    new EventCalendarView();
-    $(document).ready(function () {
-        search.engine.initialize();
-        search.searchBox();
-        $('#courseSearchBox').focus();
+    var environment = 'http://localhost:8085/lukkarimaatti';
+
+    $.ajaxSetup({ cache: false });          // force ajax call on all browsers
+
+    $.ajaxPrefilter(function (options) {
+        options.url = environment + options.url;
     });
 
+    var eventCal = new EventCalendarView();
+    $(document).ready(function () {
+        search.engine.initialize();
+        search.searchBox(eventCal);
+        $('#courseSearchBox').focus();
+    });
 });

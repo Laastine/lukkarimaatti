ed5217c1fad53068dfa359ec04672f763214ec23
Laastine <mikko.kaistinen@lut.fi>
2014-07-05 12:50:56 +0300

    Dao injected to controller     -URL shortened to <host>:<port>/lukkarimaatti/index.html

--- src/main/java/org/ltky/controller/CourseController.java
+++ src/main/java/org/ltky/controller/CourseController.java
@@ -1,16 +1,15 @@
 package org.ltky.controller;
 
-import org.ltky.dao.CourseDao;
-import org.ltky.dao.ExamDao;
-import org.ltky.model.Course;
-import org.ltky.model.Exam;
-import org.ltky.util.Util;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
 import org.apache.log4j.Logger;
+import org.ltky.dao.CourseDao;
+import org.ltky.model.Course;
+import org.ltky.util.Util;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,37 +22,47 @@ import java.util.List;
  */
 @Controller
 @RequestMapping("/rest")
-public class RestController {
-    private static final Logger LOGGER = Logger.getLogger(RestController.class);
-    private final ApplicationContext applicationContext = new ClassPathXmlApplicationContext("hibernate/hibernateConfig.xml");
-    private final CourseDao courseDao = (CourseDao) applicationContext.getBean("courseDao");
-    private final ExamDao examDao = (ExamDao) applicationContext.getBean("examDao");
+public class CourseController {
+    private static final Logger LOGGER = Logger.getLogger(CourseController.class);
     private static final int MIN = 3;
     private static final Util UTIL = new Util();
 
+    @Autowired
+    private CourseDao courseDao;
+
+    public CourseController() {}
+
     @RequestMapping(value = "/code/{code}", method = RequestMethod.GET)
-    public @ResponseBody final List<Course> getCourseCode(@PathVariable String code) {
+    public
+    @ResponseBody
+    final List<Course> getCourseCode(@PathVariable String code) {
         if (LOGGER.isDebugEnabled())
             LOGGER.debug("getCourseCode");
         return courseDao.findByCourseCode(code);
     }
 
     @RequestMapping(value = "/codes/{codes}", method = RequestMethod.GET)
-    public @ResponseBody final List<String> getLikeCourseCodes(@PathVariable String codes) {
+    public
+    @ResponseBody
+    final List<String> getLikeCourseCodes(@PathVariable String codes) {
         if (LOGGER.isDebugEnabled())
             LOGGER.debug("getLikeCourseCodes");
         return UTIL.removeDuplicates(courseDao.findCourseCodes(codes));
     }
 
     @RequestMapping(value = "/name/{courseName}", method = RequestMethod.GET)
-    public @ResponseBody final List<Course> getCourseName(@PathVariable String courseName) {
+    public
+    @ResponseBody
+    final List<Course> getCourseName(@PathVariable String courseName) {
         if (LOGGER.isDebugEnabled())
             LOGGER.debug("getCourseName");
         return courseDao.findByCourseName(courseName);
     }
 
     @RequestMapping(value = "/names/{courseNames}", method = RequestMethod.GET)
-    public @ResponseBody List<Course> getCourseNames(@PathVariable String courseNames) {
+    public
+    @ResponseBody
+    List<Course> getCourseNames(@PathVariable String courseNames) {
         if (LOGGER.isDebugEnabled())
             LOGGER.debug("getCourseNames");
         if (courseNames.length() > MIN) {
@@ -64,7 +73,9 @@ public class RestController {
     }
 
     @RequestMapping(value = "/cnames/{courseNamesandCodes}", method = RequestMethod.GET)
-    public @ResponseBody final List<Course> getCourseNamesWithCode(@PathVariable String courseNamesandCodes) {
+    public
+    @ResponseBody
+    final List<Course> getCourseNamesWithCode(@PathVariable String courseNamesandCodes) {
         if (LOGGER.isDebugEnabled())
             LOGGER.debug("getCourseNamesWithCode");
         if (courseNamesandCodes.length() > MIN) {
@@ -75,34 +86,20 @@ public class RestController {
     }
 
     @RequestMapping(value = "/department/{department}", method = RequestMethod.GET)
-    public @ResponseBody final List<Course> getDepartmentInJSON(@PathVariable String department) {
+    public
+    @ResponseBody
+    final List<Course> getDepartmentInJSON(@PathVariable String department) {
         if (LOGGER.isDebugEnabled())
             LOGGER.debug("getDepartmentInJSON");
         return courseDao.findByDepartment(department);
     }
 
     @RequestMapping(value = "/all/")
-    public @ResponseBody final List<String> getAllCourseCodes() {
+    public
+    @ResponseBody
+    final List<String> getAllCourseCodes() {
         if (LOGGER.isDebugEnabled())
             LOGGER.debug("getAllCourseCodes");
         return courseDao.findAllCourseNames();
-    }
-
-    @RequestMapping(value = "/examname/{examName}", method = RequestMethod.GET)
-    public @ResponseBody final List<Exam> getExamName(@PathVariable String examName) {
-        if (LOGGER.isDebugEnabled())
-            LOGGER.debug("getExamNames");
-        return examDao.findByExamName(examName);
-    }
-
-    @RequestMapping(value = "/examnames/{examNames}", method = RequestMethod.GET)
-    public @ResponseBody final List<Exam> getExamNames(@PathVariable final String examNames) {
-        if (LOGGER.isDebugEnabled())
-            LOGGER.debug("getExamNames");
-        if (examNames.length() > MIN) {
-            return examDao.findExamNames(examNames);
-        } else {
-            return new ArrayList<>();
-        }
     }
 }

d872932e879f718837131d883d3185df2251f5eb
Laastine <mikko.kaistinen@lut.fi>
2014-08-31 18:27:09 +0300

    Page reload with URL-params works

--- src/main/webapp/WEB-INF/app/js/views/SearchView.js
+++ src/main/webapp/WEB-INF/app/js/views/SearchView.js
@@ -2,18 +2,20 @@ define(['searchengine', 'eventcalendarview', 'text!templates/search.html'],
     function ( SearchEngine, EventCalendarView, searchTemplate) {
         'use strict';
 
+        var calendar = new EventCalendarView();
 
         var SearchView = Backbone.View.extend({
 
             initialize: function() {
                 _.bindAll(this, 'render');
                 SearchEngine.engine.initialize();
+                SearchEngine.onPageLoad(calendar);
             },
 
             render: function () {
                 this.template = _.template(searchTemplate);
                 this.$el.html(this.template({}));
-                SearchEngine.searchBox(new EventCalendarView());
+                SearchEngine.searchBox(calendar);
                 $('#courseSearchBox').focus();
                 return this;
             }

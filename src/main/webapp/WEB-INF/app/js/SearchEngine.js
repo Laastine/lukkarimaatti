49baeb4b45871c348b0abd00b266c0c83442eef4
Laastine <mikko.kaistinen@lut.fi>
2014-08-31 18:27:09 +0300

    Page reload with URL-params works

--- src/main/webapp/WEB-INF/app/js/SearchEngine.js
+++ src/main/webapp/WEB-INF/app/js/SearchEngine.js
@@ -2,7 +2,7 @@ define(['jquery', 'underscore', 'moment', 'handlebars', 'bloodhound', 'text!temp
     function ($, _, moment, Handlebars, Bloodhound, loadModal) {
         'use strict';
 
-        var courseCollection = {};
+        var courseCollection = [];
         var load = $(loadModal);
 
         var engine = new Bloodhound(
@@ -12,17 +12,7 @@ define(['jquery', 'underscore', 'moment', 'handlebars', 'bloodhound', 'text!temp
                 remote: {
                     url: '/rest/cnames/%QUERY',
                     filter: function (response) {
-                        courseCollection = $.map(response, function (course) {
-                            return {
-                                title: course.courseName,
-                                code: course.courseCode,
-                                tof: course.timeOfDay,
-                                wd: course.weekDay,
-                                wn: course.weekNumber,
-                                cr: course.classroom,
-                                t: course.type
-                            };
-                        });
+                        mapResponse(response);
                         return _.uniq(courseCollection, function (item) {
                             return item.title + item.code;
                         });
@@ -31,6 +21,20 @@ define(['jquery', 'underscore', 'moment', 'handlebars', 'bloodhound', 'text!temp
                 limit: 10
             }
         );
+
+        var mapResponse = function (response) {
+            courseCollection = $.map(response, function (course) {
+                return {
+                    title: course.courseName,
+                    code: course.courseCode,
+                    tof: course.timeOfDay,
+                    wd: course.weekDay,
+                    wn: course.weekNumber,
+                    cr: course.classroom,
+                    t: course.type
+                };
+            });
+        };
 
         var searchBox = function (eventCal) {
             var that = this;
@@ -57,96 +61,90 @@ define(['jquery', 'underscore', 'moment', 'handlebars', 'bloodhound', 'text!temp
                         return el.code === item.code;
                     });
                     if (courseCollection[0].title.length !== 0) {
-                        addCourseItem(courseCollection[0].title, courseCollection[0].code);
+                        addCourseLink(courseCollection[0].title, courseCollection[0].code);
                     }
                     load.modal('toggle');
-                    setTimeout(function(){ addDataToCalendar(eventCal);}, 300);
+                    setTimeout(function () {
+                        addDataToCalendar(eventCal);
+                    }, 300);
+                    addUrlParameter(courseCollection[0].title, courseCollection[0].code);
                 });
         };
 
         var addUrlParameter = function (courseName, courseCode) {
             var params = window.location.search;
             if (params.length > 0) {
-                console.log('not zero');
                 history.pushState(
                     {
                         name: courseName,
                         code: courseCode
-                    },
-                    "",
-                        "index.html?" + params.substring(1, params.length) + '+' + courseCode);
+                    }, "", "index.html?" + params.substring(1, params.length) + '+' + courseCode);
             } else {
-                console.log('more than zero');
                 history.pushState(
                     {
                         name: courseName,
                         code: courseCode
-                    },
-                    "",
-                        "index.html?" + params + courseCode);
+                    }, "", "index.html?" + params + courseCode);
             }
         };
 
-        var refresh = function()  {
-            var courseData = {};
+        var refresh = function (calendar) {
             var params = window.location.search;
             var courseCodes = params.substring(1, params.length).split(/[+]/);
-            console.log('courseCodes='+courseCodes);
-            if(typeof courseCodes !== 'undefined') {
-                $.ajax({
-                    url: '/rest/code/BM40A0700',
-                    type: 'GET',
-
-                    dataType: 'json',
-                    success: function(data, textStatus) {
-                        console.log('reload, data='+data+', status='+textStatus);
-                        courseData = data;
-                    },
-                    error:function(xhr, status, error) {
-                        console.log('xhr'+xhr+', status='+status+', error='+error);
+            console.log('courseCodes=' + typeof courseCodes);
+            load.modal('toggle');
+            if (courseCodes.length > 0) {
+                courseCodes.forEach(function (cc) {
+                    if (typeof cc !== 'undefined') {
+                        $.ajax({
+                            url: '/rest/code/' + cc,
+                            type: 'GET',
+                            dataType: 'json',
+                            success: function (data) {
+                                mapResponse(data);
+                                addCourseLink(data[0].courseName, data[0].courseCode);
+                                addDataToCalendar(calendar);
+                            },
+                            error: function (xhr, status, error) {
+                                console.log('xhr' + xhr + ', status=' + status + ', error=' + error);
+                            }
+                        });
+                    } else {
+                        console.log('Not a valid course code');
                     }
                 });
-            } else {
-                console.log('not valid course code');
             }
-            console.log('courseData='+courseData.courseName+', '+courseData.courseCode);
-            //addCourseItem(courseData.courseName, courseData.courseCode);
+            load.modal('hide');
         };
 
-        var addCourseItem = function (courseName, courseCode) {
+        var addCourseLink = function (courseName, courseCode) {
             var noppa = 'https://noppa.lut.fi/noppa/opintojakso/';
-            $('#courseList')
-                .append('<li id="+'+courseCode+'+" data-filtertext="' + courseName + '"><a href=' + noppa + courseCode + ' target="_blank">' + courseName + '</a> ' +
-                    '<button class="btn btn-default btn-sm" type="button">' +
-                    '<span class="glyphicon glyphicon-remove"></span>' +
-                    '</button></li>');
+            courseCode = courseCode.replace('+', '');
+            console.log('update courseCode=' + courseCode);
+            $('#courseList').append('<li id="+' + courseCode + '+" data-filtertext="' + courseName + '"><a href=' + noppa + courseCode + ' target="_blank">' + courseName + '</a></li>');
         };
 
-        var removeCourseItem = function (courseName, courseCode) {
-            $('#courseList').attr('id', courseCode).remove('<li data-filtertext="' + courseName + '"><a href=' + noppa + courseCode + ' target="_blank">' + courseName + '</a></li>');
+        var removeCourseItem = function (courseCode) {
+            $('#' + courseCode).remove();
         };
 
         var addDataToCalendar = function (calendar) {
-            function processCourse(course) {
+            courseCollection.forEach(function (course) {
                 function processWeekNumbers(weekNumber) {
-                    var hStart = course.tof.split('-')[0] || 6;
-                    var hEnd = course.tof.split('-')[1] || 6;
-                    var dateStart = moment().lang('fi').day(course.wd).week(weekNumber).hours(hStart).minutes(0).second(0).format('YYYY-MM-DDTHH:mm:ssZ');
-                    var dateEnd = moment().lang('fi').day(course.wd).week(weekNumber).hours(hEnd).minutes(0).second(0).format('YYYY-MM-DDTHH:mm:ssZ');
+                    var dateStart = moment().lang('fi').day(course.wd).week(weekNumber).hours(course.tof.split('-')[0] || 6).minutes(0).second(0).format('YYYY-MM-DDTHH:mm:ssZ');
+                    var dateEnd = moment().lang('fi').day(course.wd).week(weekNumber).hours(course.tof.split('-')[1] || 6).minutes(0).second(0).format('YYYY-MM-DDTHH:mm:ssZ');
                     calendar.createCalendarEvent(course, dateStart, dateEnd);
                 }
 
-                var weekNumber = JSON.parse('[' + course.wn + ']');
-                weekNumber.forEach(processWeekNumbers);
-            }
-
-            courseCollection.forEach(processCourse);
+                JSON.parse('[' + course.wn + ']').forEach(processWeekNumbers);
+            });
             load.modal('hide');
         };
 
         return {
             engine: engine,
-            searchBox: searchBox
+            searchBox: searchBox,
+            onPageLoad: refresh
         };
 
     });

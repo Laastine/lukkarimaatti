5802beb29c3fa42414addb8f2276abc29e9bc71f
Mikko Kaistinen <mikko.kaistinen@reaktor.com>
2015-07-19 10:49:37 +0300

    MochaTestRunnerTest works

--- src/test/java/org/ltky/mocha/MochaTestRunnerTest.java
+++ src/test/java/org/ltky/mocha/MochaTestRunnerTest.java
@@ -1,8 +1,7 @@
 package org.ltky.mocha;
 
-import org.apache.log4j.Logger;
 import org.junit.Test;
-import org.ltky.EmbeddedJetty;
+import org.ltky.JettyTestRunner;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,16 +13,15 @@ import static org.junit.Assert.assertTrue;
  * Created by laastine on 18/07/15.
  */
 public class MochaTestRunnerTest {
-    private static final Logger LOGGER = Logger.getLogger(MochaTestRunnerTest.class);
 
     @Test
     public void mochaTest() throws Exception {
-        EmbeddedJetty.startJetty();
-        String[] cmd = {"../../../node_modules/mocha-phantomjs/bin/mocha-phantomjs", "-R", "spec", "http://localhost:" + EmbeddedJetty.PORT + "/lukkarimaatti/test/runner.html"};
+        JettyTestRunner.startJetty();
+        String[] cmd = {"node_modules/mocha-phantomjs/bin/mocha-phantomjs", "-R", "spec", "http://localhost:" + JettyTestRunner.PORT + "/lukkarimaatti/test/runner.html"};
         Process ps = Runtime.getRuntime().exec(cmd);
-        assertTrue(ps.waitFor(60, TimeUnit.SECONDS));
+        assertTrue(ps.waitFor(10, TimeUnit.SECONDS));
         assertEquals(0, ps.exitValue());
-        EmbeddedJetty.stopJetty();
+        JettyTestRunner.stopJetty();
     }
 
 }

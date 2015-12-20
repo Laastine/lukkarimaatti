4eef20d6641e395bb7c8ad4f2afcc62f2c030d90
Mikko Kaistinen <mikko.kaistinen@reaktor.com>
2015-12-20 15:27:25 +0200

    react-big-calendar added

--- app/pages/frontPage.js
+++ app/pages/frontPage.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import Bacon from 'baconjs'
+import BigCalendar from 'react-big-calendar'
+import moment from 'moment'
+
+
+BigCalendar.setLocalizer(
+  BigCalendar.momentLocalizer(moment)
+)
 
 export const renderPage = applicationState =>
   <body>
   <h1>Lukkarimaatti++</h1>
+  <div>
+    <BigCalendar
+      events={[]}
+      defaultDate={new Date()}
+    />
+  </div>
   </body>
 
 export const initialState = {

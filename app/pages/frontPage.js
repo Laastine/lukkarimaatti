edda8c345574cf4f4b31eb2ab8074f2ac022354e
Mikko Kaistinen <mikko.kaistinen@reaktor.com>
2015-12-26 09:48:25 +0200

    courses added to calendar

--- app/pages/frontPage.js
+++ app/pages/frontPage.js
@@ -10,8 +10,8 @@ export const pagePath = '/'
 
 export const pageTitle = 'Lukkarimaatti++'
 
-const dayBus = new Bacon.Bus()
 const inputBus = new Bacon.Bus()
+const selectedCoursesBus = new Bacon.Bus()
 
 BigCalendar.setLocalizer(
     BigCalendar.momentLocalizer(moment)
@@ -37,13 +37,21 @@ export const applicationStateProperty = (initialState) => Bacon.update(
     searchResultsS, (applicationState, courseNames) => ({
         ...applicationState,
         courses: JSON.parse(courseNames.text)
+    }),
+    selectedCoursesBus, (applicationState, selectedCourse) => ({
+        ...applicationState,
+        selectedCourses: R.append(selectedCourse, R.filter((c) => c.course_name === selectedCourse, applicationState.courses))
     })
 ).doLog('application state')
 
 const searchList = (applicationState) =>
     R.pipe(R.map((c) => c.course_name),
-    R.uniq,
-    R.map((cname) => <div key={cname} className="search-list-coursename">{cname}</div>))(applicationState.courses)
+        R.uniq,
+        R.map((cname) =>
+            <div key={cname}
+                 className="search-list-coursename"
+                 onClick={(e) => selectedCoursesBus.push(e.target.textContent)}>{cname}</div>))
+    (applicationState.courses)
 
 export const renderPage = (applicationState) =>
     <body>
@@ -56,7 +64,7 @@ export const renderPage = (applicationState) =>
         </div>
         <div>
             <BigCalendar
-                events={[]}
+                events={addDataToCalendar(applicationState)}
                 defaultView="week"
                 min={new Date(moment(applicationState.currentDate).hours(8).minutes(0).format())}
                 max={new Date(moment(applicationState.currentDate).hours(20).minutes(0).format())}
@@ -65,3 +73,65 @@ export const renderPage = (applicationState) =>
         </div>
     </div>
     </body>
+
+const addDataToCalendar = (applicationState) => {
+    let courseToBeAdded = []
+    console.log('courses', applicationState)
+    applicationState.courses.forEach((course) => {
+        JSON.parse('[' + course.week + ']')
+            .map(function processWeekNumbers(weekNumber) {
+                let dateStart = moment()
+                    .locale('fi')
+                    .year(getYearNumber(weekNumber))
+                    .day(course.week_day)
+                    .week(weekNumber)
+                    .hours(course.time_of_day.split('-')[0] || 6).minutes(0)
+                    .seconds(0)
+                    .format('YYYY-MM-DDTHH:mm:ssZ')
+                let dateEnd = moment()
+                    .locale('fi')
+                    .year(getYearNumber(weekNumber))
+                    .day(course.week_day)
+                    .week(weekNumber)
+                    .hours(course.time_of_day.split('-')[1] || 6)
+                    .minutes(0)
+                    .seconds(0)
+                    .format('YYYY-MM-DDTHH:mm:ssZ')
+                let calendarEvent = {
+                    title: course.course_code + "-" + course.course_name,
+                    description: course.course_name + '/' + course.type + '\n' + course.classroom,
+                    start: new Date(dateStart),
+                    end: new Date(dateEnd),
+                    element: null,
+                    color: stringToColour(course.course_code),
+                    view: null,
+                    id: course.course_code + '#' + course.type
+                }
+                courseToBeAdded.push(calendarEvent)
+            })
+    })
+    return courseToBeAdded
+}
+
+const getYearNumber = (courseWeekNumber) => {
+    const isSpringSemester = moment().week() < 27
+    const week = parseInt(courseWeekNumber, 10)
+    const springCourse = (week > 0 && week < 35)
+    if (isSpringSemester) {
+        return springCourse ? moment().year() : moment().subtract(1, 'year').year()
+    } else {
+        return springCourse ? moment().add(1, 'y').year() : moment().year()
+    }
+}
+
+const stringToColour = (colorSeed) => {
+    let colour = '#', value
+    let hash = colorSeed.split("").map(function(e) {
+        colorSeed.charCodeAt(e) + ((hash << 5) - hash)
+    })
+    for (var j = 0; j < 3; j++) {
+        value = (hash >> (j * 8)) & 0xFF
+        colour += ('00' + value.toString(16)).substr(-2)
+    }
+    return colour
+}

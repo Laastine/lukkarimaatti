de1b0ae126bf5e35e173a4e5d2beaf3912c14029
Mikko Kaistinen <mikko.kaistinen@reaktor.com>
2016-01-10 13:29:42 +0200

    Github ribbon put back

--- app/pages/frontPage.js
+++ app/pages/frontPage.js
@@ -77,7 +77,7 @@ export const applicationStateProperty = (initialState) => Bacon.update(
         input
     }),
     indexBus, (applicationState, selectedIndex) => ({
-       ...applicationState,
+        ...applicationState,
         selectedIndex
     }),
     searchResultsS, (applicationState, courseNames) => ({
@@ -103,6 +103,12 @@ export const renderPage = (applicationState) =>
     <body>
     {Header(applicationState, emailBus)}
     <div className="container">
+        <a href="https://github.com/Laastine/lukkarimaatti">
+            <img style={{position: 'absolute', top: '0', right: '0', border: '0'}}
+                 src="https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67"
+                 alt="Fork me on GitHub"
+                 data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png"></img>
+        </a>
         {searchList(applicationState, inputBus, selectedCoursesBus, indexBus)}
         <div className="selected-courses-list">
             <div className="selected-courses-list-topic">Selected courses:</div>

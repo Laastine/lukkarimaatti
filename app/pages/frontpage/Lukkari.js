0b51eab530f2f0855bec1f13b41c2e0b7bc7087a
Mikko Kaistinen <mikko.kaistinen@reaktor.com>
2016-08-04 21:59:34 +0300

    Update eslint rules

---
+++ app/pages/frontpage/Lukkari.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import BigCalendar from '../calendar/index'
+import moment from 'moment'
+import {filter} from 'ramda'
+import Header from '../partials/header'
+import searchResults from '../partials/searchResults'
+import searchList from '../partials/searchList'
+import CourseParser from '../util/courseParser'
+
+require('moment/locale/fi')
+BigCalendar.momentLocalizer(moment)
+
+const stringToColor = (colorSeed) => {
+  var hash = 0, colour = '#', value
+  colorSeed.split("").forEach(function (e) {
+    hash = colorSeed.charCodeAt(e) + ((hash << 5) - hash)
+  })
+  for (var j = 0; j < 3; j++) {
+    value = (hash >> (j * 8)) & 0xFF
+    colour += ('00' + value.toString(16)).substr(-2)
+  }
+  return colour
+}
+
+const Event = ({event}) => (
+  <div style={{backgroundColor: stringToColor(event.title)}} className="calendar-event">
+    {event.title}{event.description}
+  </div>
+)
+
+const Calendar = (applicationState) =>
+  <BigCalendar
+    events={CourseParser.addDataToCalendar(applicationState)}
+    views={['month', 'week', 'day', 'agenda']}
+    popup={false}
+    components={{event: Event}}
+    onSelectEvent={(c) => {
+      const courses = filter((cc) => cc.course_code + "#" + cc.type === c.id, applicationState.selectedCourses)
+      console.log('NOT IMPLEMENTED', courses)
+    }}
+    min={new Date(moment(applicationState.currentDate).hours(8).minutes(0).format())}
+    max={new Date(moment(applicationState.currentDate).hours(20).minutes(0).format())}
+    defaultDate={new Date(moment(applicationState.currentDate).format())}
+  />
+
+class FrontPage extends React.Component {
+  render() {
+    const {state} = this.props
+    return <body>
+    {Header(state)}
+    <div className="container">
+      <a className="github-ribbon" href="https://github.com/Laastine/lukkarimaatti">
+        <img style={{position: 'absolute', top: '0px', right: '0px', border: '0px'}}
+             src="github.png"
+             alt="Fork me on GitHub">
+        </img>
+      </a>
+      {searchList(state)}
+      <div className="selected-courses-list">
+        <div className="selected-courses-list-topic">Selected courses:</div>
+        {searchResults(state)}
+      </div>
+      <div>
+        {Calendar(state)}
+      </div>
+      <div className="footer">
+        <div id="disclaimer">Use with your own risk!</div>
+        <div id="versionInfo">v1.2.8</div>
+      </div>
+    </div>
+    </body>
+  }
+}
+
+export default FrontPage

184bb26e39c0374b9026555949763de52e5d93ad
Mikko Kaistinen <mikko.kaistinen@reaktor.com>
2016-08-07 11:43:51 +0300

    Timetable and 404 page styles added

--- app/pages/notFoundPage.js
+++ app/pages/notFoundPage.js
@@ -10,8 +10,16 @@ const NotFoundPage = React.createClass({
   },
 
   render() {
-    return <div>
-      <div className='no-match'>404 Page Not Found</div>
+    return <div className="no-match-container">
+      <pre className="no-match-ascii">
+        .____           __    __                 .__                       __    __  .__
+        |    |    __ __|  | _|  | _______ _______|__| _____ _____  _____ _/  |__/  |_|__|
+        |    |   |  |  \  |/ /  |/ /\__  \\_  __ \  |/     \\__  \ \__  \\   __\   __\  |
+        |    |___|  |  /    &lt;|    &lt;  / __ \|  | \/  |  Y Y  \/ __ \_/ __ \|  |  |  | |  |
+        |_______ \____/|__|_ \__|_ \(____  /__|  |__|__|_|  (____  (____  /__|  |__| |__|
+        \/          \/    \/     \/               \/     \/     \/               </pre>
+      <div className='no-match'>404 Page Not Found.</div>
+      <a className='no-match-link' href="/">Go to lukkarimaatti</a>
     </div>
   }
 })

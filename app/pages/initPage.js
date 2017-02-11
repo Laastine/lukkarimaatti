c41f336b8c71c4bc3ff473f7a3874a4f8113ed7b
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-02-11 12:16:43 +0200

    Add ltky google analytics

--- app/pages/initPage.js
+++ app/pages/initPage.js
@@ -2,6 +2,9 @@ import React from 'react'
 import {renderToString} from 'react-dom/server'
 import App from './app'
 
+const googleAnalytics = process.env.NODE_ENV === 'production' ? `<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject'] = r;i[r]=i[r]||function(){(i[r].q = i[r].q || []).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create','UA-27895073-3','auto');ga('send','pageview');</script>` : ''
+
 export const renderFullPage = (initialState, options, renderProps) => {
   return `<!doctype html>
     <html>
@@ -38,6 +41,7 @@ export const renderFullPage = (initialState, options, renderProps) => {
         <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
         <link rel='stylesheet' href='/static/${options.cssChecksum}/styles.css'/>
         <link rel='shortcut icon' type='image/png' href='/img/favicon.png'/>
+        ${googleAnalytics}
       </head>
       <body>
         <div id='root'>${renderToString(<App state={initialState} renderProps={renderProps}></App>)}</div>

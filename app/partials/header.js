12bb0ca5b3e4f1b442fbcc2eee56fddbf70ab5b5
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-02-23 21:10:29 +0200

    Extract partials to own functions in header element

--- app/partials/header.js
+++ app/partials/header.js
@@ -2,36 +2,38 @@ import React from 'react'
 import {Link} from 'react-router'
 import {appState} from '../store/lukkariStore'
 
+const sendButton = (waitingAjax) => waitingAjax ?
+  <img className='modal-ajax-spinner' src='/spinner.gif'/> :
+  <button type='button' id='saveId' className='modal-button' data-dismiss='modal'
+          onClick={(e) => {
+            appState.dispatch({
+              type: 'SEND_EMAIL',
+              waitingAjax: true,
+              email: e.target.previousElementSibling.value
+            })
+          }}>
+    Send
+  </button>
+
+const modal = (isModalOpen, waitingAjax) => isModalOpen ? <div className='modal-dialog'>
+    <div>
+      <div id='saveClose' onClick={() => {
+        appState.dispatch({type: 'SAVE_MODAL', isModalOpen: false})
+      }} className='close'>X</div>
+      <div>Send course selection URL to your email.</div>
+      <form className='modal-input-container'>
+        <input type='email' className='modal-input' id='saveEmail' placeholder='Email'/>
+        {sendButton(waitingAjax)}
+      </form>
+    </div>
+  </div> : null
+
 class Header extends React.Component {
   render() {
     const {isModalOpen, waitingAjax} = this.props.state
-    const sendButton = waitingAjax ?
-      <img className='modal-ajax-spinner' src='/spinner.gif'/> :
-      <button type='button' id='saveId' className='modal-button' data-dismiss='modal'
-              onClick={(e) => {
-                appState.dispatch({
-                  type: 'SEND_EMAIL',
-                  waitingAjax: true,
-                  email: e.target.previousElementSibling.value
-                })
-              }}>
-        Send
-      </button>
-    const modal = isModalOpen ? <div className='modal-dialog'>
-      <div>
-        <div id='saveClose' onClick={() => {
-          appState.dispatch({type: 'SAVE_MODAL', isModalOpen: false})
-        }} className='close'>X
-        </div>
-        <div>Send course selection URL to your email.</div>
-        <form className='modal-input-container'>
-          <input type='email' className='modal-input' id='saveEmail' placeholder='Email'/>
-          {sendButton}
-        </form>
-      </div>
-    </div> : null
+
     return <div className='header-container'>
-      {modal}
+      {modal(isModalOpen, waitingAjax)}
       <Link className='header-element header-link' to={'/'}>Lukkarimaatti++</Link>
       <a id='saveModalButton' className='header-element header-element-link' onClick={() => {
         appState.dispatch({type: 'SAVE_MODAL', isModalOpen: true})

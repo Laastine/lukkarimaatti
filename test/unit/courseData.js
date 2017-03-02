59ce564fa06b61281c26d918b35d763a4aff7020
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-03-02 21:13:22 +0200

    Mock data file to node format

--- test/unit/courseData.js
+++ test/unit/courseData.js
@@ -1,4 +1,4 @@
-export const courseData = `<!DOCTYPE html PUBLIC " -//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+const courseData = `<!DOCTYPE html PUBLIC " -//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
   <html xmlns='http://www.w3.org/1999/xhtml'>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
@@ -7553,7 +7553,7 @@ width = '100%' > <tr>
   < / tr >
   < / table >`
 
-export const kikeCourseData = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+const kikeCourseData = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns='http://www.w3.org/1999/xhtml'>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
@@ -16334,3 +16334,8 @@ span.label-0-0-0{background-color: rgb(192,192,192); }
 </td>
 </tr>
 </table>`
+
+module.exports = {
+  courseData,
+  kikeCourseData
+}

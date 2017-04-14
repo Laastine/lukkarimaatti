ab066a094bc76d34c0305615dd403987415585fa
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-04-14 09:17:28 +0300

    Enhance browser error logger

--- app/client.js
+++ app/client.js
@@ -6,8 +6,30 @@ import Routes from './pages/routes'
 import App from './pages/app'
 
 window.onload = () => {
-  browserHistory.listen(() => {
-  })
+  window.onerror = function(message, file, line, col, error) {
+    function serializeError(object) {
+      const alt = {}
+      if (object) {
+        Object.getOwnPropertyNames(object).forEach((key) => {
+          alt[key] = object[key]
+        }, object)
+      }
+      return alt
+    }
+
+    function sendMessageToServer(msg) {
+      const xhr = new XMLHttpRequest()
+      xhr.open('POST', '/errors')
+      xhr.setRequestHeader('Content-Type', 'application/json')
+      xhr.send(JSON.stringify(msg))
+    }
+
+    sendMessageToServer({
+      error: {message: message, file: file, line: line, col: col, error: serializeError(error)},
+      userAgent: window.navigator.userAgent
+    })
+    return false
+  }
 
   match({routes: Routes, history: browserHistory}, (error, redirectLocation, renderProps) => {
     render(<App renderProps={renderProps}></App>, document.getElementById('root'))
--- app/routes/errorLogger.js
+++ app/routes/errorLogger.js
@@ -5,14 +5,9 @@ const Logger = require('../logger')
 const errorLoggerRoutes = express.Router()
 
 errorLoggerRoutes.post('', (req, res) => {
-  const errorMsg = {
-    msg: req.body.error.message,
-    line: req.body.error.line,
-    userAgent: req.body.navigator.userAgent,
-    language: req.body.navigator.language,
-    systemLanguage: req.body.navigator.systemLanguage
-  }
-  Logger.error('Browser error', errorMsg)
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  const {message, line} = req.body.error
+  Logger.error('Browser error:', message, 'line:', line, 'userAgent:', req.body.userAgent, 'IP:', ip)
   res.send('""')
   res.status(201).end()
 })

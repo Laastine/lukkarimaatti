a528ad5506127ea4899f5d7bf75a066a10518232
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-08-04 13:39:00 +0300

    Change link div elements to a elements

--- app/partials/header.js
+++ app/partials/header.js
@@ -12,11 +12,11 @@ class Header extends React.Component {
   render() {
     return <div className='header-container'>
       <Modal state={this.props.state}/>
-      <div className='header-element header-link' href='/' onClick={onLinkClick}>Lukkarimaatti++</div>
+      <a className='header-element header-link' href='/' onClick={onLinkClick}>Lukkarimaatti++</a>
       <a id='saveModalButton' className='header-element header-element-link' onClick={() => {
         appState.dispatch({type: 'SAVE_MODAL', isModalOpen: true})
       }}>Save</a>
-      <div id='catalogButton' className='header-element header-element-link' href='/catalog' onClick={onLinkClick}>Catalog</div>
+      <a id='catalogButton' className='header-element header-element-link' href='/catalog' onClick={onLinkClick}>Catalog</a>
     </div>
   }
 }

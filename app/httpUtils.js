1b26fefce5495364cfef4ea81de02ed8ddcaca7a
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-08-21 19:36:45 +0300

    Increase HSTS max age to half year

--- app/httpUtils.js
+++ app/httpUtils.js
@@ -1,12 +1,10 @@
-import appConfig from './config'
-
 export const frameOptions = () => (req, res, next) => {
   res.setHeader('X-Frame-Options', 'DENY')
   next()
 }
 
 export const hsts = () => {
-  const maxAge = Math.round(60 * 60 * 24)
+  const maxAge = Math.round(60 * 60 * 24 * 183) //Half year
   return (req, res, next) => {
     res.setHeader('Strict-Transport-Security', `max-age=${maxAge}; includeSubDomains; preload`)
     next()

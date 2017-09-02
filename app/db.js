3d8fe9cdfa3a8132b83be9eb44ef2a6b81e459b6
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-09-02 10:01:41 +0300

    Add SQL sanitizer

--- app/db.js
+++ app/db.js
@@ -8,26 +8,66 @@ const Logger = require('./logger')
 
 const client = pgp(appConfig.postgresUrl)
 
-const buildInsertQueryString = (courseBatch) =>
-  reduce((a, course) => `${a}INSERT INTO course (` +
-    'course_code, course_name, week, week_day, time_of_day, classroom, type, department, teacher, misc, group_name) ' +
-    `VALUES ('${
-      course.course_code}','${
-      course.course_name}','${
-      course.week}','${
-      course.week_day}','${
-      course.time_of_day}','${
-      course.classroom}','${
-      course.type}','${
-      course.department}','${
-      course.teacher}','${
-      course.misc}','${
-      course.group_name}');`, ''
+const charsRegex = /[\0\b\t\n\r\x1a"'\\]/g
+const charsMap = {
+  '\0': '\\0',
+  '\b': '\\b',
+  '\t': '\\t',
+  '\n': '\\n',
+  '\r': '\\r',
+  '\x1a': '\\Z',
+  '"': '\\"',
+  '\'': '\\\'',
+  '\\': '\\\\'
+}
+
+const sanitize = (value) => {
+  let chunkIndex = charsRegex.lastIndex = 0
+  let escapedVal = ''
+  let match
+
+  while ((match = charsRegex.exec(value))) {
+    escapedVal += value.slice(chunkIndex, match.index) + charsMap[match[0]]
+    chunkIndex = charsRegex.lastIndex
+  }
+
+  if (chunkIndex === 0) {
+    return `${value}`
+  }
+
+  if (chunkIndex < value.length) {
+    return `${escapedVal}${value.slice(chunkIndex)}`
+  }
+
+  return escapedVal
+}
+
+const SQL = (queryParts, ...values) =>
+  queryParts.reduce((query, queryPart, i) => {
+    const valueExists = i < values.length
+    const text = query + queryPart
+
+    return valueExists ? text + sanitize(values[i]) : text
+  }, '')
+
+const buildInsertQueryString = courseBatch =>
+  reduce((a, course) => `${a}INSERT INTO course (course_code, course_name, week, week_day, time_of_day, classroom, type, department, teacher, misc, group_name) VALUES ('${
+    course.course_code}','${
+    course.course_name}','${
+    course.week}','${
+    course.week_day}','${
+    course.time_of_day}','${
+    course.classroom}','${
+    course.type}','${
+    course.department}','${
+    course.teacher}','${
+    course.misc}','${
+    course.group_name}');`, ''
     , courseBatch)
 
 module.exports = {
 
-  isTableInitialized: (table) => client.query('SELECT ${columns^} IS NOT NULL AS exists', {columns: `to_regclass('${table}')`})
+  isTableInitialized: table => client.query('SELECT ${columns^} IS NOT NULL AS exists', {columns: `to_regclass('${table}')`})
     .then((e) => prop('exists')(e[0]))
     .catch((err) => Logger.error('isTableInitialized error', err.stack)),
 
@@ -46,41 +86,40 @@ module.exports = {
     .then(() => client.any('CREATE INDEX course_name_search ON course (course_name)'))
     .catch((err) => Logger.error('initializeDb error', err.stack)),
 
-  getCourseByName: (courseName) => client.query('SELECT * FROM course WHERE LOWER(course_name) LIKE $1', [`%${courseName}%`]),
+  getCourseByName: (courseName) => client.query(`SELECT * FROM course WHERE LOWER(course_name) LIKE '%${courseName.toLocaleLowerCase()}%'`),
 
-  getCourseByCodeAndGroup: (code, group) => client.query('SELECT * FROM course WHERE course_code = ${code} AND group_name = ${group}', {code: `${code}`, group: `${group}`}),
+  getCourseByCodeAndGroup: (code, group) => client.query(SQL`SELECT * FROM course WHERE course_code = '${code}' AND group_name = '${group}'`),
 
-  getCourseByCode: (code) => client.query('SELECT * FROM course WHERE course_code = ${code}', {code: `${code}`}),
+  getCourseByCode: code => client.query(SQL`SELECT * FROM course WHERE course_code = '${code}'`),
 
-  getCourseByDepartment: (department) =>
-    client.query('SELECT * FROM course WHERE department = ${department}', {
-      department: `${department}`
-    }),
+  getCourseByDepartment: department => client.query(SQL`SELECT * FROM course WHERE department = '${department}'`),
 
-  prefetchCoursesByCode: (params) => {
-    const insertGroupCondition = groupName => groupName ? ` AND group_name = '${groupName}'` : ''
-    const insertCodeCondition = (courseCode) => courseCode ? ` OR course_code = '${courseCode}'` : ''
+  prefetchCoursesByCode: params => {
+    const insertGroupCondition = groupName => groupName ? SQL` AND group_name = '${groupName}'` : ''
+    const insertCodeCondition = courseCode => courseCode ? SQL` OR course_code = '${courseCode}'` : ''
     if (params.length > 0) {
-      let query = `SELECT * FROM course WHERE course_code = '${params[0].courseCode}'${insertGroupCondition(params[0].groupName)}`
+      let query = SQL`SELECT * FROM course WHERE course_code = '${params[0].courseCode}'${insertGroupCondition(params[0].groupName)}`
       if (params.length > 1) {
         query += reduce((a, b) => a + insertCodeCondition(b.courseCode) + insertGroupCondition(b.groupName), '', tail(params))
       }
       return client.query(query)
-        .then((result) => result)
-        .catch((error) => Logger.info('prefetchCoursesByCode error', error))
+        .then(result => result)
+        .catch(error => Logger.info('prefetchCoursesByCode error', error))
     }
     return []
   },
 
-  insertCourse: (courseBatch) => client.query(buildInsertQueryString(courseBatch))
-    .catch((error) => Logger.error('buildInsertQueryString error', error.stack)),
+  insertCourse: courseBatch => client.query(buildInsertQueryString(courseBatch))
+    .catch(error => Logger.error('buildInsertQueryString error', error.stack)),
 
   updateKikeCourseCode: (courseCode, courseName) =>
-    client.query(`UPDATE course SET course_code = '${courseCode}' WHERE course_name ILIKE '${courseName}' AND department = 'kike';`)
-      .catch((error) => Logger.error('updateCourseCode error', error.stack)),
+    client.query(SQL`UPDATE course SET course_code = '${courseCode}' WHERE course_name ILIKE '${courseName}' AND department = 'kike';`)
+      .catch(error => Logger.error('updateCourseCode error', error.stack)),
 
   cleanCourseTable: () => client.query('TRUNCATE TABLE course;'),
 
-  cleanKikeCourseTable: () => client.query('DELETE FROM course WHERE department = \'kike\';')
+  cleanKikeCourseTable: () => client.query('DELETE FROM course WHERE department = \'kike\';'),
+
+  SQL
 }
 

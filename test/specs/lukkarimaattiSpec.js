09c645d099dd18b7752b162878af27df1a0a5617
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-09-04 19:50:13 +0300

    Add course catalog removal test case

--- test/specs/lukkarimaattiSpec.js
+++ test/specs/lukkarimaattiSpec.js
@@ -1,6 +1,6 @@
 /* eslint-env mocha, jquery */
 /* global browserBack, click, clickText, expect, getSelectedCoursesFromUrl,
-  LukkarimaattiPage, S, setInputValue, testFrame, waitUntil */
+  LukkarimaattiPage, S, setInputValue, testFrame, waitUntil, wait */
 describe('Lukkarimaatti UI navigation', () => {
   const page = LukkarimaattiPage()
   before(() => page.openPage(() => {
@@ -92,8 +92,33 @@ describe('Lukkarimaatti UI navigation', () => {
       .then(() => {
         const selectedCourses = S('.selected-courses-list .search-list-element').map(e => e.textContent)
         expect(selectedCourses.some(e => e === 'CT60A2411 - Olio-ohjelmointiX')).to.equal(true)
-        expect(selectedCourses.some((e) => e === 'BL20A1600 - Smart GridsX')).to.equal(true)
+        expect(selectedCourses.some(e => e === 'BL20A1600 - Smart GridsX')).to.equal(true)
         expect(getSelectedCoursesFromUrl()).to.deep.equal(['BL20A1600', 'CT60A2411'])
+        done()
+      })
+  })
+
+  it('Test course removal from catalog', (done) => {
+    waitUntil(() => page.openPage(() => {
+      const el = testFrame().document.getElementsByClassName('rbc-calendar')
+      return el && el.length > 0
+    }, 'http://localhost:8080/?courses=BL20A1600+CT60A2411'))()
+      .then(click('a#catalogButton'))
+      .then(waitUntil(() => S('.department-link-selected').length === 1 && S('.department-course-list').length > 0))
+      .then(clickText('li.department-course', /CT60A2411[\w -]+/))
+      .then(wait(500))
+      .then(click('div:nth-child(7) > a.department-link'))
+      .then(waitUntil(() => S('div:nth-child(7) > a.department-link-selected').length > 0 &&
+        S('.department-course-list').length > 0))
+      .then(clickText('li.department-course', /BL20A1600[\w -]+/))
+      .then(wait(500))
+      .then(clickText('a', /Lukkarimaatti\+\+/))
+      .then(waitUntil(() => $(S('.rbc-calendar')[0]).is(':visible')))
+      .then(() => {
+        const selectedCourses = S('.selected-courses-list .search-list-element').map(e => e.textContent)
+        expect(selectedCourses.some(e => e === 'CT60A2411 - Olio-ohjelmointiX')).to.equal(false)
+        expect(selectedCourses.some(e => e === 'BL20A1600 - Smart GridsX')).to.equal(false)
+        expect(getSelectedCoursesFromUrl()).to.deep.equal(['http://localhost:8080/'])
         done()
       })
   })

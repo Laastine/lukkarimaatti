408641e961ac3182691d4188664bf07cfb87034e
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2018-07-29 20:25:58 +0300

    Refactor DB scripts

--- db/database.sh
+++ db/database.sh
@@ -2,28 +2,31 @@
 
 set -eou pipefail
 
-function print_status {
-  if [[ $1 -eq 0 ]]; then
-    printf "\n[OK]\n"
-  else
-    printf "\n[FAIL]\n"
-  fi
-}
+DBNAME="postgres"
+DATADIR="lukkarimaatti-db"
 
 if [ "$#" -eq 0 ]; then
-  echo "Usage: $0 [start|stop]"
+  echo "Usage: $0 [init|start|stop]"
+elif [ "$1" = "init" ]; then
+  echo "Initializing database"
+  initdb $DATADIR
+
+  postgres -D $DATADIR > db.log 2>&1 &
+  echo $! > postgres.pid
+  sleep 3
+  createuser postgres
+  psql $DBNAME -f init.sql
+
 elif [ "$1" = "start" ]; then
   echo "Starting PostgreSQL server"
   postgres -D lukkarimaatti-db
   EXIT_STATUS=$?
 
   printf 'PostgreSQL server started'
-  print_status EXIT_STATUS
 elif [ "$1" = "stop" ]; then
   echo "Stoping PostgreSQL server"
   pg_ctl -D lukkarimaatti-db stop -s -m fast
   EXIT_STATUS=$?
 
   printf 'PostgreSQL server stopped'
-  print_status EXIT_STATUS
 fi
